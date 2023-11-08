beda2b2cb0e0ca97ca4761b4e7522931b5482b2b
56Gustavo <56gustavogomes@gmail.com>
2023-11-07 22:12:59 -0300

    horario funcionando

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ app.use(
 );
 
 const produtos = [
-    { id: 1, nome: 'Arroz', tipo: "alimento", preco: 25 },
-    { id: 2, nome: 'Feijao', tipo: "alimento", preco: 15 },
-    { id: 3, nome: 'Bife', tipo: "alimento", preco: 40 },
-    { id: 4, nome: 'Detergente', tipo: "Produto de limpeza", preco: 25 },
-    { id: 5, nome: 'Água sanitária', tipo: "Produto de limpeza", preco: 15 },
-    { id: 6, nome: 'Sabão em pó', tipo: "Produto de limpeza", preco: 40 },
+    { id: 1, nome: 'Arroz', tipo: 'alimento', preco: 25 },
+    { id: 2, nome: 'Feijao', tipo: 'alimento', preco: 15 },
+    { id: 3, nome: 'Bife', tipo: 'alimento', preco: 40 },
+    { id: 4, nome: 'Detergente', tipo: 'Produto de limpeza', preco: 25 },
+    { id: 5, nome: 'Água sanitária', tipo: 'Produto de limpeza', preco: 15 },
+    { id: 6, nome: 'Sabão em pó', tipo: 'Produto de limpeza', preco: 40 },
 ];
 
 app.get('/produtos', (req, res) => {
@@ -41,6 +41,8 @@ app.get('/adicionar/:id', (req, res) => {
         if (!req.session.carrinho) {
             req.session.carrinho = [];
         }
+        const dataHora = new Date();
+        produto.horarioAdicao = `${dataHora.toLocaleDateString('pt-BR')} ${dataHora.toLocaleTimeString('pt-BR')}`;
         req.session.carrinho.push(produto);
     }
 
@@ -51,23 +53,19 @@ app.get('/carrinho', (req, res) => {
     const carrinho = req.session.carrinho || [];
     const total = carrinho.reduce((acc, produto) => acc + produto.preco, 0);
 
-    // Obter o tipo do último produto adicionado
     const ultimoTipoAdicionado = carrinho.length > 0 ? carrinho[carrinho.length - 1].tipo : null;
-
-    // Filtrar os produtos no carrinho com o mesmo tipo do último produto adicionado
-    const produtosMesmoTipo = produtos.filter((produto) => produto.tipo === ultimoTipoAdicionado);
 
     res.send(`
         <h1>Carrinho de Compras</h1>
         <ul>
             ${carrinho.map(
-                (produto) => `<li>${produto.nome} - ${produto.preco} ${produto.tipo}</li>`
+                (produto) => `<li>${produto.nome} - ${produto.preco} ${produto.tipo} (Adicionado em: ${produto.horarioAdicao})</li>`
             ).join('')}
         </ul>
 
         <h2>Produtos do Mesmo Tipo do Último Adicionado:</h2>
         <ul>
-            ${produtosMesmoTipo.map(
+            ${produtos.filter((produto) => produto.tipo === ultimoTipoAdicionado).map(
                 (produto) => `<li>${produto.nome} - ${produto.preco} ${produto.tipo}</li>`
             ).join('')}
         </ul>
